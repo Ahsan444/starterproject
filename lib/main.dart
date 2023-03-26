2a5450c6c09cb86de2a0ecfd5852db39253ee03b
ahsan <ahsanshabbir445@gmail.com>
2023-03-26 15:05:29 +0500

    splash_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:starter_project/AppModule/splash_screen/splash_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'AppModule/splash_screen/view/splash_screen.dart';
+import 'AppModule/splash_screen/viewmodel/splash_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
@@ -11,13 +17,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => SplashProvider(),
+            ),
+          ],
+          child: MaterialApp(
+            title: 'Mvvm Provider',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const SplashScreen(),
+          ),
+        );
+      },
+      child: const SplashScreen(),
+      // child: box.read('isRememberMe') ==1 ? const DashboardScreen() :const LoginScreen(),
     );
   }
 }
-
