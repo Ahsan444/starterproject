a03b708939321c41718cdc27721bbb02ac7679f6
ahsan <ahsanshabbir445@gmail.com>
2023-03-27 19:24:47 +0500

    route folder added

--- lib/AppModule/splash_screen/view/splash_screen.dart
+++ lib/AppModule/splash_screen/view/splash_screen.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SplashScreen extends StatelessWidget {
+import '../viewmodel/splash_provider.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late final AnimationController controller;
+
+  @override
+  void initState() {
+    var splashProvider = Provider.of<SplashProvider>(context, listen: false);
+    controller = AnimationController(vsync: this);
+    splashProvider.startTime();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
--- lib/AppModule/splash_screen/viewmodel/splash_provider.dart
+++ lib/AppModule/splash_screen/viewmodel/splash_provider.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class SplashProvider extends ChangeNotifier {
+  startTime() async {
+    var _duration = const Duration(seconds: 4);
+    return Timer(_duration, () {
 
+    });
+  }
 }
